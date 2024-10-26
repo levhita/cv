3f1fa7c6858a827e9cf7960407a8630b40b8e1d1
Levhita <levhita@photoshelter.com>
2024-10-26 02:27:52 -0600

    * added education and all experiences

--- generatePDF.js
+++ generatePDF.js
@@ -5,13 +5,16 @@ const page = await browser.newPage();
 
 await page.setViewport({ width: 1080, height: 1024 });
 
-await page.goto('http://127.0.0.1:5174/?print',);
+await page.goto('http://127.0.0.1:5173/?print',);
 
 await page.waitForNetworkIdle({});
 
+const body = await page.$('html');
+const bodyBoundingBox = await body.boundingBox();
+
 await page.pdf({
   path: 'public/levhita-cv.pdf',
-  height: `3001px`,
+  height: `${bodyBoundingBox.height * .7}px`,
   printBackground: true,
   waitForFonts: true,
   scale: .7
